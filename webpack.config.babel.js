121592c1e2352be1089ac9d55c79de92d5fb89ee
sam0sva1 <konstantus@live.ru>
2017-06-08 14:20:17 +0700

    ET removes postcss-cssnext

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -26,7 +26,8 @@ export default {
                         sourceMap: true,
                         parser: 'sugarss',
                         plugins: [
-                            require('postcss-cssnext')
+                            require('autoprefixer'),
+                            require('postcss-nested')
                         ]
                     }
                 }
