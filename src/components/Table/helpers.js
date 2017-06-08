0fc4a6fc9eb3502a17f666d3e1be961b6ea287b8
sam0sva1 <konstantus@live.ru>
2017-06-08 14:19:46 +0700

    ET makes functions external

---
+++ src/components/Table/helpers.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export const formPages = (items, numberOfItems) => {
+    const pages = []
+    items.forEach((item, idx) => {
+        const i = Math.floor(idx / numberOfItems)
+        if (!pages[i]) pages.push([])
+        pages[i].push(item)
+    })
+    return pages
+}
+
+export const getPages = (items, part) => {
+    return formPages(getTableRows(items), part)
+}
+
+export const getTableRows = (items) => {
+    return items.map(({ name, description, organization, origin }) => (
+        <tr key={name}>
+            <td>{name}</td>
+            <td>{origin}</td>
+            <td>{organization}</td>
+            <td>{description}</td>
+        </tr>
+    ))
+}
+
+export const getFilteredItems = (items, filters) => {
+    const activeFilters = filters.filter(f => f.isActive)
+    if (activeFilters.length) {
+        return items.filter(({ origin }) => {
+            for (const one of activeFilters) {
+                if (one.name === origin) {
+                    return true
+                }
+            }
+            return false
+        })
+    } else {
+        return items
+    }
+}
+
+export const searchByToken = (token, items) => {
+    const trimedToken = token.toLowerCase().trim()
+    return items.filter((one) => {
+        const decision = one.name.toLowerCase().indexOf(trimedToken)
+        return decision !== -1
+    })
+}
+
+export const prepareItems = (items, filters, searchToken) => {
+    const token = searchToken.trim()
+    const filteredItems = getFilteredItems(items, filters)
+    return token ? searchByToken(token, filteredItems) : filteredItems
+}
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -7,62 +7,8 @@ import Paging from 'components/Paging'
 import Listing from 'components/Listing'
 import tools from 'Tools'
 import { changePage, updateSearchToken } from 'actions'
+import { prepareItems, getPages } from './helpers'
 import './Table.sss'
-
-const formPages = (items, numberOfItems) => {
-    const pages = []
-    items.forEach((item, idx) => {
-        const i = Math.floor(idx / numberOfItems)
-        if (!pages[i]) pages.push([])
-        pages[i].push(item)
-    })
-    return pages
-}
-
-const getPages = (items, part) => {
-    return formPages(getTableRows(items), part)
-}
-
-const getTableRows = (items) => {
-    return items.map(({ name, description, organization, origin }) => (
-        <tr key={name}>
-            <td>{name}</td>
-            <td>{origin}</td>
-            <td>{organization}</td>
-            <td>{description}</td>
-        </tr>
-    ))
-}
-
-const getFilteredItems = (items, filters) => {
-    const activeFilters = filters.filter(f => f.isActive)
-    if (activeFilters.length) {
-        return items.filter(({ origin }) => {
-            for (const one of activeFilters) {
-                if (one.name === origin) {
-                    return true
-                }
-            }
-            return false
-        })
-    } else {
-        return items
-    }
-}
-
-const searchByToken = (token, items) => {
-    const trimedToken = token.toLowerCase().trim()
-    return items.filter((one) => {
-        const decision = one.name.toLowerCase().indexOf(trimedToken)
-        return decision !== -1
-    })
-}
-
-const prepareItems = (items, filters, searchToken) => {
-    const token = searchToken.trim()
-    const filteredItems = getFilteredItems(items, filters)
-    return token ? searchByToken(token, filteredItems) : filteredItems
-}
 
 @connect(
     state => ({
