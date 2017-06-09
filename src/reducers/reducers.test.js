649f22a2bcf96a0330d80d4b61638c51c6dd5379
sam0sva1 <konstantus@live.ru>
2017-06-09 12:25:00 +0700

    ET adds a test for filters reducer

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,38 +1,40 @@
 import itemsReducer from './items'
+import filtersReducer from './filters'
 
-describe('Reducers - items', () => {
-    const initState = {
+const initItemsState = {
+    name: 'Экспонат',
+    city: 'Уральск',
+    country: 'Россия',
+    organization: 'ООО Производитель',
+    desctiption: 'Некоторое описание',
+    origin: 'Уральск, Россия'
+}
+
+const firstItem = {
+    type: 'CREATE_ITEM',
+    item: {
         name: 'Экспонат',
         city: 'Уральск',
         country: 'Россия',
         organization: 'ООО Производитель',
-        desctiption: 'Некоторое описание',
-        origin: 'Уральск, Россия'
+        desctiption: 'Некоторое описание'
     }
-    
-    const firstItem = {
-        type: 'CREATE_ITEM',
-        item: {
-            name: 'Экспонат',
-            city: 'Уральск',
-            country: 'Россия',
-            organization: 'ООО Производитель',
-            desctiption: 'Некоторое описание'
-        }
-    }
+}
 
-    const secondItem = {
-        type: 'CREATE_ITEM',
-        item: {
-            name: 'Арбуз',
-            city: 'Ереван',
-            country: 'Россия',
-            organization: 'ОАО Изготовитель',
-            desctiption: 'Инетерсный очерк'
-        }
+const secondItem = {
+    type: 'CREATE_ITEM',
+    item: {
+        name: 'Арбуз',
+        city: 'Ереван',
+        country: 'Россия',
+        organization: 'ОАО Изготовитель',
+        desctiption: 'Инетерсный очерк'
     }
+}
 
-    test('Test 1 - CREACTE ITEM', () => {
+describe('Reducers - items', () => {
+
+    test('Test 1 - no action', () => {
         expect(
             itemsReducer(undefined, {})
         ).toEqual([])
@@ -41,12 +43,12 @@ describe('Reducers - items', () => {
     test('Test 2 - CREACTE ITEM with action', () => {
         expect(
             itemsReducer([], firstItem)
-        ).toEqual([initState])
+        ).toEqual([initItemsState])
     })
 
     test('Test 3 - CREACTE ITEM and sorting', () => {
         expect(
-            itemsReducer([initState], secondItem)
+            itemsReducer([initItemsState], secondItem)
         ).toEqual([{
             name: 'Арбуз',
             city: 'Ереван',
@@ -54,7 +56,53 @@ describe('Reducers - items', () => {
             organization: 'ОАО Изготовитель',
             desctiption: 'Инетерсный очерк',
             origin: 'Ереван, Россия'
-        }, initState])
+        }, initItemsState])
+    })
+
+})
+
+const initFiltersState = [
+    { name: 'Калининград, Россия', isActive: false },
+    { name: 'Вильнюс, Литва', isActive: false }
+]
+
+describe('Reducers - filters', () => {
+
+    test('Test 1 - no action', () => {
+        expect(
+            filtersReducer(undefined, {})
+        ).toEqual([])
+    })
+
+    test('Test 2 - TOGGLE FILTER', () => {
+        expect(
+            filtersReducer(initFiltersState, {
+                type: 'TOGGLE_FILTER',
+                label: 'Вильнюс, Литва'
+            })
+        ).toEqual([
+            { name: 'Калининград, Россия', isActive: false },
+            { name: 'Вильнюс, Литва', isActive: true }
+        ])
+    })
+
+    test('Test 3 - CREATE ITEM and sorting', () => {
+        expect(
+            filtersReducer(initFiltersState, {
+                type: 'CREATE_ITEM',
+                item: {
+                    name: 'Арбуз',
+                    city: 'Ереван',
+                    country: 'Россия',
+                    organization: 'ОАО Изготовитель',
+                    desctiption: 'Инетерсный очерк'
+                }
+            })
+        ).toEqual([
+            { name: 'Вильнюс, Литва', isActive: false },
+            { name: 'Ереван, Россия', isActive: false },
+            { name: 'Калининград, Россия', isActive: false }
+        ])
     })
 
 })
