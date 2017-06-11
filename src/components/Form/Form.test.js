e7f5b9eb49e6bfe1feb418899dd642587f05323a
sam0sva1 <konstantus@live.ru>
2017-06-11 09:28:19 +0700

    ET adds a test for Form component

---
+++ src/components/Form/Form.test.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { shallow, mount } from 'enzyme'
+import Form from './index'
+import configureStore from '../../configureStore'
+
+function init() {
+    const Wrapper = ({ children }) => (
+        <div>
+            {children}
+        </div>
+    )
+
+    const wrapper = mount(
+        <Form store={configureStore()} />
+    )
+
+    return wrapper
+}
+
+describe('components', () => {
+
+    describe('Form', () => {
+
+        test('renders it self and collupsing button', () => {
+            const wrapper = init()
+
+            expect(wrapper.find('.form')).toHaveLength(1)
+            expect(wrapper.find('button')).toHaveLength(1)
+        })
+
+        test('Button shows form inputs', () => {
+            const wrapper = init()
+
+            expect(wrapper.find('input')).toHaveLength(0)
+            wrapper.find('button').simulate('click')
+            expect(wrapper.find('input')).toHaveLength(4)
+            expect(wrapper.find('textarea')).toHaveLength(1)
+        })
+
+        test('submit button rendered disabled', () => {
+            const wrapper = init()
+
+            wrapper.find('button').simulate('click')
+            const submitButton = wrapper.find('.form__btn')
+            expect(submitButton).toHaveLength(1)
+            expect(submitButton.getNode().disabled).toBe(true)
+        })
+
+    })
+})
